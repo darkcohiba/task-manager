6e8d1b931272fea58bb85a2f182e1e22a643fddb
Sam <samuel.waters@gmail.com>
2023-01-11 20:03:30 -0700

    chore: base

---
+++ mongodb.js
@@ -0,0 +1,13 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = "mongodb://127.0.0.1:27017";
+const databaseName = "task-manager";
+
+//create
+
+//read
+
+//update
+
+//delete

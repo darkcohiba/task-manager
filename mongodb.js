56fb049f41b3692d891647b63917576f322b67a0
Sam <samuel.waters@gmail.com>
2023-01-12 09:55:49 -0700

    feat:added create function

--- mongodb.js
+++ mongodb.js
@@ -4,8 +4,22 @@ const MongoClient = mongodb.MongoClient;
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
 
-//create
 
+
+//create
+MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) =>  {
+    if (error) {
+        return console.log('unable to connect')
+    }
+
+    const db = client.db(databaseName);
+
+    db.collection("users").insertOne({
+        name: 'Sam Waters',
+        age: 30
+    })
+
+});
 //read
 
 //update

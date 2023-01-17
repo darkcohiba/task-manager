93be0df8a76c44487e78267dd791fdc4f0d8972c
Sam <samuel.waters@gmail.com>
2023-01-17 12:00:32 -0700

    added task schema and first task to be added

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -8,7 +8,9 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
     // useCreateIndex: true
 })
 
-
+////////////////////////////////////////////////////////////////
+///////////////////User Configuration///////////////////////////
+////////////////////////////////////////////////////////////////
 const userSchema = new Schema({
     name: {
         type: String,
@@ -38,22 +40,68 @@ const userSchema = new Schema({
             message: '{VALUE} is not supported'
         }
     },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 7,
+        validate(value){
+            if (value.toLowerCase().includes('password')){
+                throw new Error('Password cannot contain "password"')
+            }
+        }
+
+    }
 });
+
+
 
 const User = mongoose.model('User', userSchema);
 
-const me = new User({
-    name: 'Sam Waters',
-    sex: 'male',
-    age: 30,
-    email: 'SAMUEL.waters@gmail.com'
+// const me = new User({
+//     name: 'Sam Waters',
+//     sex: 'male',
+//     age: 30,
+//     email: 'SAMUEL.waters@gmail.com',
+//     password: 'flatiron098'
+// })
+
+
+// me.save().then(() => {
+//     console.log(me)
+// }).catch((error) => {
+//     console.log(error)
+// })
+
+// console.log(User.find({}))
+
+
+////////////////////////////////////////////////////////////////
+///////////////////Task Configuration///////////////////////////
+////////////////////////////////////////////////////////////////
+
+const taskSchema = new Schema({
+    description: {
+        type: String,
+        required: [true, 'please tell us about your task or else put it in a different db!!'],
+        trim: true,
+    },
+    completed: {
+        type: Boolean,
+        required: true,
+        default: false
+    }
+})
+
+const Task = mongoose.model('Task', taskSchema);
+
+const laundry = new Task({
+    description: 'do laundry',
 })
 
 
-me.save().then(() => {
-    console.log(me)
+laundry.save().then(() => {
+    console.log(laundry)
 }).catch((error) => {
     console.log(error)
 })
-
-// console.log(User.find({}))

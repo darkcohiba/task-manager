36a5f072ca15dca34602974e1a6823fc504d355a
Sam <samuel.waters@gmail.com>
2023-01-21 14:25:02 -0700

    chore: added post to express

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000
 
+app.post('/users', (req, res) => {
+    res.send('testing!')
+})
+
 app.listen(port, ()=> {
     console.log('listening on port ' + port);
 });

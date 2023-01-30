769eef04ec0e66a570907490d87af589b3a023ae
Sam <samuel.waters@gmail.com>
2023-01-30 09:43:43 -0700

    feat: added get request for tasks and task by id

--- src/index.js
+++ src/index.js
@@ -17,13 +17,11 @@ app.get('/users', (req, res) => {
 })
 
 app.get('/users/:id', (req, res) => {
-
     const _id = req.params.id
     User.findById(_id).then((user) => {
         if (!user){
-            res.status(404).send(`User not found`)
+            res.status(404).send()
         }
-
         res.send(user)
     }).catch((e) => {
         res.status(500).send(e)
@@ -38,6 +36,26 @@ app.post('/users', (req, res) => {
     }).catch((e) => {
         res.status(400).send(e)
     })
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(500).send('error: ' + e)
+    });
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+    Task.findById(_id).then((task) => {
+        if (!task){
+            res.status(404).send()
+        }
+        res.send(task)
+    }).catch((e) => {
+        res.status(500).send('error: ' + e)
+    });
 })
 
 app.post('/tasks', (req, res) => {

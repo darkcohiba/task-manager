cf61849db9e08b0c40c4aa2beeeb6cb5f7db74ef
Sam <samuel.waters@gmail.com>
2023-02-02 16:18:30 -0700

    cleaned

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose')
 
-
-thankYouCards.push()
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
     useNewUrlParser: true,
     // useStrictQuery: false,

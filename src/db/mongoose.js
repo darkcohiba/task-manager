48c7f9bce8fa0350522038b18e544cd59b20ae94
Sam <samuel.waters@gmail.com>
2023-02-02 16:18:15 -0700

    more

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,16 @@
 const mongoose = require('mongoose')
 
+
+thankYouCards.push()
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
     useNewUrlParser: true,
+    // useStrictQuery: false,
+    // useFindandModify: false
     // useCreateIndex: true
 })
+
+
+
+// mongoose.set('useFindAndModify', false);
+// mongoose.set('useCreateIndex', true);
+// mongoose.set('useUnifiedTopology', true);
